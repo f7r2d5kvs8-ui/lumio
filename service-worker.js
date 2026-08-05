e86499796b72156dccc947a69c378ed3d6de9f39
f7r2d5kvs8-ui <f7r2d5kvs8@privaterelay.appleid.com>
2026-08-05 10:34:26 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,8 @@
+const CACHE = 'lumio-v4';
+const ASSETS = ['./', './index.html', './styles.css', './app.js', './data/languages.js', './modules/storage.js'];
+self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
+self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', event => event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request))));
 const CACHE = 'lumio-v3';
 const ASSETS = ['./', './index.html', './styles.css', './app.js', './data/languages.js', './modules/storage.js'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
