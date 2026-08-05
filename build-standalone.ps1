56eeb7938c03121272e3e4fbd9fb380f696c65e5
Ali Hosseini <hosseini.ee@icloud.com>
2026-08-05 12:01:06 +0200

    Show only active tracing fragment

--- build-standalone.ps1
+++ build-standalone.ps1
@@ -3,6 +3,7 @@ $index = [IO.File]::ReadAllText((Join-Path $root 'index.template.html'))
 $css = [IO.File]::ReadAllText((Join-Path $root 'styles.css'))
 $css += "`r`n" + [IO.File]::ReadAllText((Join-Path $root 'games.css'))
 $css += "`r`n" + [IO.File]::ReadAllText((Join-Path $root 'letters.css'))
+$css += "`r`n" + [IO.File]::ReadAllText((Join-Path $root 'trace-fix.css'))
 $languages = [IO.File]::ReadAllText((Join-Path $root 'data\languages.js')) -replace 'export const ', 'const '
 $writingPaths = [IO.File]::ReadAllText((Join-Path $root 'data\writing-paths.js')) -replace 'export const ', 'const '
 $storage = [IO.File]::ReadAllText((Join-Path $root 'modules\storage.js')) -replace 'export function ', 'function '
@@ -12,6 +13,7 @@ $script = $languages + "`r`n" + $writingPaths + "`r`n" + $storage + "`r`n" + $cl
 $index = $index -replace '<link rel="stylesheet" href="styles.css">', "<style>`r`n$css`r`n</style>"
 $index = $index -replace '\s*<link rel="stylesheet" href="games.css">', ''
 $index = $index -replace '\s*<link rel="stylesheet" href="letters.css">', ''
+$index = $index -replace '\s*<link rel="stylesheet" href="trace-fix.css">', ''
 $index = $index -replace '<script type="module" src="app.js"></script>', "<script>`r`n$script`r`n</script>"
 [IO.File]::WriteAllText((Join-Path $root 'index.html'), $index, (New-Object Text.UTF8Encoding($false)))
 Write-Output 'Standalone Lumio build generated.'
